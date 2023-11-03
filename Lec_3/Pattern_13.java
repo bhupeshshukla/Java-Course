889ad2b8e75aa7a493b4f642497c38323955039d
Bhupesh Shukla <bhupeshshuklaindia012@gmail.com>
2023-11-04 00:08:30 +0530

    pattern

---
+++ Lec_3/Pattern_13.java
@@ -0,0 +1,30 @@
+package Lec_3;
+import java.util.*;
+public class Pattern_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int row = 1;
+		int star = 1;
+		while (row <= 2 * n - 1) {
+			// star
+			int i = 1;
+			while (i <= star) {
+				System.out.print("* ");
+				i++;
+			}
+			// mirror
+			if (row < n) {
+				star++;
+			} else {
+				star--;
+			}
+			row++;
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ Lec_3/Pattern_17.java
@@ -0,0 +1,42 @@
+package Lec_3;
+import java.util.Scanner;
+public class Pattern_17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int row = 1;
+		int star = n / 2;
+		int space = 1;
+		while (row <= n) {
+			// star
+			int i = 1;
+			while (i <= star) {
+				System.out.print("* ");
+				i++;
+			}
+			// space
+			int j = 1;
+			while (j <= space) {
+				System.out.print("  ");
+				j++;
+			}
+			int k = 1;
+			while (k <= star) {
+				System.out.print("* ");
+				k++;
+			}
+			if (row <= n / 2) {
+				star--;
+				space += 2;
+			} else {
+				star++;
+				space -= 2;
+			}
+			row++;
+			System.out.println();
+		}
+	}
+
+}
