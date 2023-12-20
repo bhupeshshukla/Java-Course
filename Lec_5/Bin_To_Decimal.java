ee8008992034e579d2772935a7eb3a627e199b4c
Bhupesh Shukla <bhupeshshuklaindia012@gmail.com>
2023-12-21 00:31:40 +0530

    fun & datatypes

---
+++ Lec_5/Bin_To_Decimal.java
@@ -0,0 +1,22 @@
+package Lec_5;
+
+public class Bin_To_Decimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 10101;
+		int sum = 0;
+		int mul = 1;
+		while (n > 0) {
+			int rem = n % 10;
+			sum = sum + rem * mul;
+			n = n / 10;
+			mul = mul * 2;
+
+		}
+		System.out.println(sum);
+	
+	}
+
+}
